ca126feefc47e1f6287d620bf04af221996d3040
mondherhammemi <hammemimondher721@gmail.com>
2022-12-17 13:12:32 +0100

    service entity is created

---
+++ src/Entity/Service.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ServiceRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ServiceRepository::class)]
+class Service
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $serviceName = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getServiceName(): ?string
+    {
+        return $this->serviceName;
+    }
+
+    public function setServiceName(string $serviceName): self
+    {
+        $this->serviceName = $serviceName;
+
+        return $this;
+    }
+}
